aaa18d2dc3aaf6afb4b3b2ed35b18886c4699353
Pablo Núñez <pablonete@github.com>
2024-08-19 18:27:07 +0000

    Configure Next.js to deploy on GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/arenaysal-web",
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
