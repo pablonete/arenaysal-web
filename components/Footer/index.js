134c2ffca49f999e3e4bd70a07576ef79efd86ac
Pablo Núñez <pablonete@github.com>
2024-08-20 09:29:49 +0000

    Upgrade to next 14

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -5,11 +5,11 @@ const Footer = ({}) => {
   return (
     <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
       Made With ❤ by{" "}
-      <Link href="https://github.com/pablonete">
-        <a className="underline underline-offset-1">Pablonete</a>
+      <Link className="underline underline-offset-1" href="https://github.com/pablonete">
+        Pablonete
       </Link>{" based on a template by "}
-      <Link href="https://github.com/chetanverma16">
-        <a className="underline underline-offset-1">Chetan Verma</a>
+      <Link className="underline underline-offset-1" href="https://github.com/chetanverma16">
+        Chetan Verma
       </Link>
     </h1>
   );
